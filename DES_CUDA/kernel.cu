df3c2c9e285a16c2900ecf9a06edbb6eac315137
Tom <neanderman@gmail.com>
2014-04-04 13:29:50 -0500

    Fixed mapping threads to DES blocks

--- DES_CUDA/kernel.cu
+++ DES_CUDA/kernel.cu
@@ -8,57 +8,65 @@
 #include <string.h>
 #include <stdint.h>
 #define DES_BLOCKSIZE 8
+#define BLOCKS_PER_LAUNCH 5
+#define THREADS_PER_BLOCK 512
+
+void launchKernel(char *h_chunk, int chunkSize, FILE *fp_out);
 
 __global__ void desBlockOperation(char *in, char *out, int n)
 {
-	int i = threadIdx.x * DES_BLOCKSIZE;
+	//Map the thread id and block id to the DES block
+	int i = ((blockIdx.x * THREADS_PER_BLOCK) + threadIdx.x) * DES_BLOCKSIZE;
 	if(i < n)
 	{
-		for (int j = i; j < i+DES_BLOCKSIZE && j < n; j++)
+		for (int j = i; j < i + DES_BLOCKSIZE && j < n; j++)
 		{
-			out[j] = in[j] + j%DES_BLOCKSIZE;
+			out[j] = in[j] + j % DES_BLOCKSIZE;
 		}
 	}
 }
 
 int main()
 {
+
 	char *fileName = "Text.txt";
 	//Open the file for reading
 	struct _stat stat;
 	_stat(fileName,&stat);
-	int fileSize = stat.st_size;
+	long fileSize = stat.st_size;
 	FILE *fp_in = fopen(fileName , "r");
-	char *h_text;
-	//TODO: Read the file in multiple chunks
+	char *newFileName = "out.txt";
+	FILE *fp_out = fopen(newFileName, "ab");
+	char *h_chunkData;
+	//Each kernel launch can handle 512 threads each operating on DES_BLOCKSIZE bytes
+	///So read the file in DES_BLOCKSIZE * THREADS_PER_BLOCK * BLOCKS_PER_LAUNCH chunks
 	if(fp_in && &stat)
 	{
-		h_text = (char *)malloc(fileSize);
-		char *s = (char *)malloc(sizeof(char *));
-		fread(h_text, fileSize, 1, fp_in);
-		fclose(fp_in);
-	}
-	if(h_text){
-		//Allocate memory on the device
-		char *d_text = NULL;
-		cudaMalloc((void **) &d_text, fileSize);
-		//Copy input data to device
-		cudaMemcpy(d_text, h_text, fileSize, cudaMemcpyHostToDevice);
-		//Invoke kernel
-		int threadsPerBlock = (fileSize + DES_BLOCKSIZE -1) / DES_BLOCKSIZE;
-		desBlockOperation<<<1, threadsPerBlock>>>(d_text, d_text, fileSize);
-		//Copy data from device to host
-		cudaMemcpy(h_text, d_text, fileSize, cudaMemcpyDeviceToHost);
 		
-		//Write the output to a file
-		char *newFileName = "out.txt";
-		FILE *fp_out = fopen(newFileName, "w");
-		if(fp_out)
+		int chunkSize = DES_BLOCKSIZE * THREADS_PER_BLOCK * BLOCKS_PER_LAUNCH;
+		h_chunkData = (char *)malloc(chunkSize);
+		//Read the file in 1 chunk at a time, until fread returns something other than the chunk size
+		int lastChunkSize;
+		do
 		{
-			fwrite(h_text, fileSize, 1, fp_out);
+			lastChunkSize = fread(h_chunkData, 1, chunkSize, fp_in);
+			launchKernel(h_chunkData, lastChunkSize, fp_out);
 		}
+		while(lastChunkSize == chunkSize);
+		fclose(fp_in);
+		
 	}
     return 0;
 }
 
+///Allocate device memory and invoke kernel, writing results to file
+void launchKernel(char *h_chunk, int chunkSize, FILE *fp_out)
+{
+	char *d_chunk = NULL;
+	cudaMalloc((void **) &d_chunk, chunkSize);
+	cudaMemcpy(d_chunk, h_chunk, chunkSize, cudaMemcpyHostToDevice);
+	desBlockOperation<<<BLOCKS_PER_LAUNCH, THREADS_PER_BLOCK>>>(d_chunk, d_chunk, chunkSize);
+	cudaMemcpy(h_chunk, d_chunk, chunkSize, cudaMemcpyDeviceToHost);
+	fwrite(h_chunk, chunkSize, 1, fp_out);
+}
 
